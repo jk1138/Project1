470673f1b1bd6e793eba8d5bdf283ad6fcdfb9e3
Lindsay Iveson <ivesonlindsay@gmail.com>
2019-07-13 14:28:08 -0400

    added on click event when search button is clicked

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,25 @@
-console.log ("hi");
+// on click, of the search button, build the queryURL to get & log the search results
+
+$("#submit").on("click", function (search) {
+    // keep page from refreshing on submit click
+    search.preventDefault();
+    
+    // runs queryURL function
+    buildQueryURL(search)
+    console.log($(this).val());
+
+    $.ajax ({
+        url: buildQueryURL,
+        method: "GET"
+    })
+    .then(function (response){
+        var results = response._embedded.data
+
+        for (var i = 0; results.length; i++) {
+            console.log(results[i]);
+        }
+    
+    })
+});
+
+

b17bad8fbca6e7e3997332caa50c77c236b703b8
Joscandy Nunez <joscandy@Joscandys-MacBook.local>
2019-07-18 19:03:46 -0400

    got search results to display on div

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,58 +1,104 @@
-console.log ("hi");
+
+
+console.log("hi");
 console.log("testing git pushes");
-console.log ("whats going on?!?!?!?!")
+console.log("whats going on?!?!?!?!")
+let weatherDate = ''
+function buildQueryURL() {
 
-function buildQueryURL(){
+  apiKey = "&apikey=WJCRVoCmP83xVzLx0AUyj20UyFAAKNbS";
 
-    apiKey = "&apikey=WJCRVoCmP83xVzLx0AUyj20UyFAAKNbS";
+  var localeSearch = "&city=" + $("#location").val().trim();
+  var dateSearch = "&startDateTime=" + $("#startDate").val().trim();
+  var keywordSearch = "&keyword=" + $("#keyword").val().trim();
+  var endDate = "&endDateTime=" + $("#endDate").val().trim();
+  console.log(dateSearch)
 
-    var localeSearch ="&city=" + $("#location").val().trim();
-    var dateSearch ="&startDateTime=" + $("#startdate").val().trim();
-    var keywordSearch ="&keyword=" + $("#keyword").val().trim();
+  queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?" + apiKey + keywordSearch + localeSearch + dateSearch + endDate + "&radius=15&units=miles";
 
-    queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?" + apiKey + keywordSearch + localeSearch + dateSearch + "&radius=15&units=miles";
+  $.ajax({
+    url: "http://104.200.17.235:8081/cors/",
+    method: "POST",
+    contentType: "application/json",
+    data: JSON.stringify({
+      url: queryURL
+    })
+  }).then(function (response) {
+    function weatherQuery() {
 
-    $.ajax({
-        url: "http://104.200.17.235:8081/cors/",
-        method: "POST",
-        contentType: "application/json",
-        data: JSON.stringify({
-          url: queryURL
-        })
-      }).then(function (response) {
-        console.log(JSON.parse(response))
-      
-        var results = parsedResponse._embedded.events;
+      // var weatherQueryURL = "'" + "https://api.openweathermap.org/data/2.5/weather?q=" + localeSearch + "&APPID=73aa9f49c204f7ee3c55d47346f4224a" + "'"
 
-        for (var i = 0; results.length; i++) {
-            console.log(results[i]);
-        }
-      })
+      // $.ajax({
+      //   url: weatherQueryURL,
+      //   method: "GET"
+      // }).then(function (response) {
+      //   console.log(response);
+      // });
+    }
+    weatherQuery();
+    $('#showEvents').empty()
+    for (var i = 0; i < 20; i++) {
 
-};
 
-$("#eventfinder").on("click", function(){
-    buildQueryURL();
+
+      var data = JSON.parse(response)
+      var results = data._embedded.events
+      console.log(results[i]);
+
+      var data = results[i];
+   
+      weatherDate = data.dates.start.localDate
+
+      var pName = $("<p>").text(data.name).addClass('col')
+      var pDates = $("<p>").text(data.dates.start.localDate).addClass('col')
+      var pTime = $("<p>").text(data.dates.start.localTime).addClass('col')
+      var pVenue = $("<p>").text(data._embedded.venues[0].name).addClass('col')
+      var img = $("<img>").attr("src", data.images[0].url).addClass('style')
+      var divImg = $("<div>").append(img).addClass('col')
+
+
+
+      //  .addClass("row")
+      var newRow = $("<div>").addClass('row')
+      $(newRow).append(divImg)
+      $(newRow).append(pName);
+      $(newRow).append(pDates);
+      $(newRow).append(pTime);
+      $(newRow).append(pVenue);
+      $("#showEvents").append(newRow);
+
+    }
+
+    console.log(JSON.parse(response))
+
+
+    var data = JSON.parse(response)
+    var results = data._embedded.events
+
+    var results = data._embedded.events;
+
+    for (var i = 0; i < 20; i++) {
+      console.log(results[i]);
+    }
+
+  })
+}
+$("#eventFinder").on("click", function () {
+  buildQueryURL();
 })
 
-for (var i = 0; i < results.length; i++) {
-  console.log(results[i]);
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyDh5DPIlsYtfh4x-PeXsQrNS2l20zxPxPs",
+  authDomain: "project1-ec674.firebaseapp.com",
+  databaseURL: "https://project1-ec674.firebaseio.com",
+  projectId: "project1-ec674",
+  storageBucket: "",
+  messagingSenderId: "580863475932",
+  appId: "1:580863475932:web:a7813153dd7c134e"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-  var data = results[i];
-  var pName = $("<p>").html(data.name);
-  var pDates = $("<p>").html(data.dates.start.localDate);
-  var pTime = $("<p>").html(data.dates.start.localTime);
-  var pVenue = $("<p>").html(data._embedded.venues[0].name)
-  var img = $("<img>").attr("src",data.images[0].url)
-  // var divImg = $("<div>").append(img)
-  
 
-  
 
-  var newRow = $("<div>").addClass("row")
-  $(newRow).append(img)
-  $(newRow).append(pName);
-  $(newRow).append(pDates);
-  $(newRow).append(pTime);
-  $(newRow).append(pVenue);
-  $("#eventResults").append(newRow);

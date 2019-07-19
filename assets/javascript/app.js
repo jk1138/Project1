ae34ad6c1eb5eade63268cc707e6e59af7ee1190
Joscandy Nunez <joscandy@Joscandys-MacBook.local>
2019-07-18 20:31:57 -0400

    just trying to pull Colin's new work

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,67 +1,53 @@
 let weatherDate = '';
-
 function buildQueryURL() {
   apiKey = "&apikey=WJCRVoCmP83xVzLx0AUyj20UyFAAKNbS";
-
   var localeSearch = "&city=" + $("#location").val();
-  var dateSearch = "&startDateTime=" + $("#startdate").val();
+  // var dateSearch = "&startDateTime=" + $("#startdate").val();
   var keywordSearch = "&keyword=" + $("#eventFinder").val();
-
-  queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?" + apiKey + keywordSearch + localeSearch + dateSearch + "&radius=15&units=miles";
-
-
+  queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?" + apiKey + keywordSearch + localeSearch + "" + "&radius=15&units=miles";
   $.ajax({
     url: 'https://cors-anywhere.herokuapp.com/' + queryURL,
     headers: { 'X-Requested-With': 'XMLHttpRequest' },
   }).then(function (response) {
     console.log(response);
-
     // console.log(JSON.parse(response))
-
-    var data = response
-
-  $.ajax({
-    url: "http://104.200.17.235:8081/cors/",
-    method: "POST",
-    contentType: "application/json",
-    data: JSON.stringify({
-      url: queryURL
-    })
-  }).then(function (response) {
-    function weatherQuery() {
-
-      // var weatherQueryURL = "'" + "https://api.openweathermap.org/data/2.5/weather?q=" + localeSearch + "&APPID=73aa9f49c204f7ee3c55d47346f4224a" + "'"
-
-      // $.ajax({
-      //   url: weatherQueryURL,
-      //   method: "GET"
-      // }).then(function (response) {
-      //   console.log(response);
-      // });
-    }
-    weatherQuery();
+    // var data = response
+  // });
+  // $.ajax({
+  //   url: "http://104.200.17.235:8081/cors/",
+  //   method: "POST",
+  //   contentType: "application/json",
+  //   data: JSON.stringify({
+  //     url: queryURL
+  //   })
+  // }).then(function (response) {
+    // function weatherQuery() {
+    //   // var weatherQueryURL = "'" + "https://api.openweathermap.org/data/2.5/weather?q=" + localeSearch + "&APPID=73aa9f49c204f7ee3c55d47346f4224a" + "'"
+    //   // $.ajax({
+    //   //   url: weatherQueryURL,
+    //   //   method: "GET"
+    //   // }).then(function (response) {
+    //   //   console.log(response);
+    //   // });
+    // }
+    // weatherQuery();
     $('#showEvents').empty()
     for (var i = 0; i < 20; i++) {
-      var data = JSON.parse(response)
+      var data = response
       var results = data._embedded.events
       console.log(results[i]);
-
       var data = results[i];
    
       weatherDate = data.dates.start.localDate
       var pButton = $("<button>").addClass("btn btn-outline-success btn-sm").attr("type","button").attr("data-toggle","modal").attr("data-target", "#exampleModal").html("check weather!").addClass("weather_button")
-      var pName = $("<p>").text(data.name).addClass('col-md-2')
-      var pDates = $("<p>").text(data.dates.start.localDate).addClass('col-md-2')
-      var pTime = $("<p>").text(data.dates.start.localTime).addClass('col-md-1')
-      var pVenue = $("<p>").text(data._embedded.venues[0].name).addClass('col-md-1')
+      var pName = $("<p>").text(data.name).addClass('col')
+      var pDates = $("<p>").text(data.dates.start.localDate).addClass('col')
+      var pTime = $("<p>").text(data.dates.start.localTime).addClass('col')
+      var pVenue = $("<p>").text(data._embedded.venues[0].name).addClass('col')
       var img = $("<img>").attr("src", data.images[0].url).addClass('style')
-      var divImg = $("<div>").append(img).addClass('col-md-2')
-
-
-
-
+      var divImg = $("<div>").append(img).addClass('col')
       //  .addClass("row")
-      var newRow = $("<div>").addClass('row')
+      var newRow = $("<div>").addClass('row').attr('hover', 'background-color:yellow')
       $(newRow).append(divImg)
       $(newRow).append(pName);
       $(newRow).append(pDates);
@@ -69,17 +55,13 @@ function buildQueryURL() {
       $(newRow).append(pVenue);
       $(newRow).append(pButton)
       $("#showEvents").append(newRow);
-
     }
-
-    console.log(JSON.parse(response))
-
+    console.log(response)
   })
-}
+};
 $("#eventFinder").on("click", function () {
   buildQueryURL();
 })
-
 // Your web app's Firebase configuration
 var firebaseConfig = {
   apiKey: "AIzaSyDh5DPIlsYtfh4x-PeXsQrNS2l20zxPxPs",
@@ -92,3 +74,6 @@ var firebaseConfig = {
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+
+
+
